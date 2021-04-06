7fc09cd3d96b1fce82003af1633491ac00537586
benntust <67628098+benntust@users.noreply.github.com>
2021-04-07 01:23:26 +0800

    Add files via upload

--- IG_liker.py
+++ IG_liker.py
@@ -18,20 +18,21 @@ class InstagramBot:
         driver.get("https://www.instagram.com/")
         #等username元素出現，至多30sec
         WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.NAME, 'username')))
-        #定位
+        #定位帳號密碼輸入欄
         username_input = driver.find_elements_by_name('username')[0]
         password_input = driver.find_elements_by_name('password')[0]
         #輸入帳號密碼
         username_input.send_keys(self.username)
         password_input.send_keys(self.password)
-        #等login button可以被按下
+        #等待login button可以被按下
         WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="loginForm"]/div/div[3]/button/div')))
-        #定位
+        #定位login button
         login_click = driver.find_elements_by_xpath('//*[@id="loginForm"]/div/div[3]/button/div')[0]
         #按下login button
         login_click.click()
-        WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="react-root"]/section/main/div/div/div/div/button')))
-        '''dontcare1=driver.find_elements_by_xpath('//*[@id="react-root"]/section/main/div/div/div/div/button')[0]
+        time.sleep(5)
+        '''WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="react-root"]/section/main/div/div/div/div/button')))
+        dontcare1=driver.find_elements_by_xpath('//*[@id="react-root"]/section/main/div/div/div/div/button')[0]
         dontcare1.click()
         WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[4]/div/div/div/div[3]/button[2]')))
         dontcare2=driver.find_elements_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]')[0]
@@ -50,8 +51,8 @@ class InstagramBot:
                 time.sleep(2)
                 # get tags
                 hrefs_in_view = driver.find_elements_by_tag_name('a')
-                # finding relevant hrefs
-                #'.com/p/' is a component of post address
+                # get hyperlink
+                # '.com/p/' is a component of post address
                 hrefs_in_view = [elem.get_attribute('href') for elem in hrefs_in_view
                                  if '.com/p/' in elem.get_attribute('href')]            
                 # building list of unique photos
@@ -63,8 +64,6 @@ class InstagramBot:
         for pic_href in pic_hrefs:
             driver.get(pic_href)
             time.sleep(2)
-            # scroll webpage
-            #driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
             try:
                 #like_button = driver.find_elements_by_class_name('fr66n')
                 like_button = driver.find_elements_by_xpath(".//*[@class='fr66n']/button")
